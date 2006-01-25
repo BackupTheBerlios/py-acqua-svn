a0912c36f17649b656d7e8afc204835398a68a45
pyacqua <pyacqua@14f0a01c-cf0a-0410-8b7d-a03980e64962>
2006-01-25 06:19:33 +0000

    0.7

--- trunk/files/vasca.py
+++ trunk/files/vasca.py
@@ -24,13 +24,16 @@ pygtk.require('2.0')
 import gtk
 from data import *
 import finestre
-#from pysqlite2 import dbapi2 as sqlite
+from finestre import *
+import gobject
+from pysqlite2 import dbapi2 as sqlite
 #from sqlite import *
 #data1 = data()
-
+as = "Data/db"
 class win6:
 	def __init__(self):
-		self.win = finestre.win(450, 538, "Py-Acqua Vasche", 5)
+		
+		self.win = finestre.win(450, 600, "Py-Acqua Vasche", 5)
 		
 		self.table = gtk.Table(2, 2, gtk.FALSE)
 		self.win.add(self.table)
@@ -38,12 +41,17 @@ class win6:
 		self.notebook = gtk.Notebook()
 		self.notebook.set_tab_pos(gtk.POS_TOP)
 		self.table.attach(self.notebook, 0, 2, 0, 1)
+		self.notebook.connect("switch-page", self.funziona_vasca)
 		self.notebook.show()
 		
 		#Pagina Cancella
+		
 		self.label16 = gtk.Label("Cancella")
 		self.fixed1 = gtk.Fixed()
-		self.notebook.prepend_page(self.fixed1)
+		self.notebook.prepend_page(self.fixed1, self.label16)
+		
+		
+		
 		self.spinbutton2 = gtk.SpinButton(None)
 		self.spinbutton2.set_value(0)
 		self.fixed1.put(self.spinbutton2, 250, 13)
@@ -71,31 +79,9 @@ class win6:
 		self.fixed1.put(self.label37, 90, 205)
 		self.entry22 = gtk.Entry(30)
 		self.fixed1.put(self.entry22, 210, 205)
-		#self.label38 = gtk.Label("No3")
-		#self.fixed1.put(self.label38, 90, 237)
-		#self.entry23 = gtk.Entry(30)
-		#self.fixed1.put(self.entry23, 210, 237)
-		#self.label39 = gtk.Label("Conducibilita'")
-		#self.fixed1.put(self.label39, 90, 269)
-		#self.entry24 = gtk.Entry(30)
-		#self.fixed1.put(self.entry24, 210, 269)
-		#self.label40 = gtk.Label("Ammoniaca")
-		#self.fixed1.put(self.label40, 90, 301)
-		#self.entry25 = gtk.Entry(30)
-		#self.fixed1.put(self.entry25, 210, 301)
-		#self.label41 = gtk.Label("Ferro")
-		#self.fixed1.put(self.label41, 90, 333)
-		#self.entry26 = gtk.Entry(30)
-		#self.fixed1.put(self.entry26, 210, 333)
-		#self.label42 = gtk.Label("Rame")
-		#self.fixed1.put(self.label42, 90, 365)
-		#self.entry27 = gtk.Entry(30)
-		#self.fixed1.put(self.entry27, 210, 365)
-		#self.label43 = gtk.Label("Fosfati")
-		#self.fixed1.put(self.label43, 90, 397)
-		#self.entry28 = gtk.Entry(30)
-		#self.fixed1.put(self.entry28, 210, 397)
+		
 		#Bottoni
+		
 		self.button10 = gtk.Button("Visualizza")
 		self.button10.connect("clicked", self.visualizza_vasca)
 		self.fixed1.put(self.button10, 70, 440)
@@ -108,7 +94,9 @@ class win6:
 		self.button13 = gtk.Button("Cancella")
 		self.button13.connect("clicked", self.cancella_nuovo_vasca)
 		self.fixed1.put(self.button13, 270, 475)
+		
 		#Cancella Database
+		
 		self.label44 = gtk.Label("Cancella tutto il database dei test")
 		self.fixed1.put(self.label44, 100, 515)
 		self.button14 = gtk.Button("Cancella")
@@ -116,27 +104,36 @@ class win6:
 		self.fixed1.put(self.button14, 170, 540)
 		
 		#Pagina Visualizza
+		
 		self.label17 = gtk.Label("Visualizza")
 		self.scrolled = gtk.ScrolledWindow()
 		self.scrolled.set_policy(gtk.POLICY_ALWAYS, gtk.POLICY_ALWAYS)
-		self.notebook.prepend_page(self.scrolled)
-		self.listore = gtk.ListStore(int, str, str, str, str, str, str, str, str, str, str, str, str)
+		
+		self.notebook.prepend_page(self.scrolled, self.label17)
+		
+		
+		self.listore = gtk.ListStore(int, str, str, str, str, str, str, str, str)
 		self.treeview1 = gtk.TreeView(self.listore)
-		self.tvcolumn = gtk.TreeViewColumn('Colonna1')
-		self.tvcolumn1 = gtk.TreeViewColumn('Colonna2')
-		self.treeview1.append_column(self.tvcolumn)
-		self.treeview1.append_column(self.tvcolumn1)
 		self.scrolled.add_with_viewport(self.treeview1)
 		
 		#Pagina Inserisci
+		
 		self.label18 = gtk.Label("Inserisci")
 		self.fixed3 = gtk.Fixed()
-		self.notebook.prepend_page(self.fixed3)
+		self.notebook.prepend_page(self.fixed3, self.label18)
 		self.label19 = gtk.Label("Id")
 		self.fixed3.put(self.label19, 90, 13)
-		self.spinbutton1 = gtk.SpinButton(None)
-		self.spinbutton1.set_value(0)
+		
+		####per spinbutton#####
+		
+		self.spinbutton1 = gtk.SpinButton(climb_rate=0, digits=0)
+		self.spinbutton1.set_digits(0)
+		self.spinbutton1.set_wrap(True)
+		self.spinbutton1.set_range(0, 100)
+		#self.spinbutton1.set_value(13)
 		self.fixed3.put(self.spinbutton1, 210, 10)
+		
+		
 		self.checkbutton1 = gtk.CheckButton("Data di oggi")
 		#self.checkbutton1.connect("toggled", self.data, "Data di oggi")
 		self.fixed3.put(self.checkbutton1, 280, 13)
@@ -146,9 +143,24 @@ class win6:
 		self.fixed3.put(self.entry4, 210, 40)
 		self.label21 = gtk.Label("Vasca")
 		self.fixed3.put(self.label21, 90, 77)
+		
+		###per il comboentry1#####
+		
+		store = gtk.ListStore(gobject.TYPE_STRING)
+		store.append (["Dolce"])
+		store.append (["Dolce Tropicale"])
+		store.append (["Marino"])
+		store.append (["Marino Tropicale"])
+		
 		self.comboentry1 = gtk.ComboBoxEntry(model=None, column=-1)
+		self.comboentry1.set_model(store)
+		self.comboentry1.set_text_column(0)
+		
 		self.fixed3.put(self.comboentry1, 210, 77)
 		self.label22 = gtk.Label("Nome")
+		
+		
+		
 		self.fixed3.put(self.label22, 90, 109)
 		self.entry6 = gtk.Entry(30)
 		self.fixed3.put(self.entry6, 210, 109)
@@ -172,22 +184,6 @@ class win6:
 		self.fixed3.put(self.label27, 90, 269)
 		self.entry11 = gtk.Entry(30)
 		self.fixed3.put(self.entry11, 210, 269)
-		#self.label28 = gtk.Label("Ammoniaca")
-		#self.fixed3.put(self.label28, 90, 301)
-		#self.entry12 = gtk.Entry(30)
-		#self.fixed3.put(self.entry12, 210, 301)
-		#self.label29 = gtk.Label("Ferro")
-		#self.fixed3.put(self.label29, 90, 333)
-		#self.entry13 = gtk.Entry(30)
-		#self.fixed3.put(self.entry13, 210, 333)
-		#self.label30 = gtk.Label("Rame")
-		#self.fixed3.put(self.label30, 90, 365)
-		#self.entry14 = gtk.Entry(30)
-		#self.fixed3.put(self.entry14, 210, 365)
-		#self.label31 = gtk.Label("Fosfati")
-		#self.fixed3.put(self.label31, 90, 397)
-		#self.entry15 = gtk.Entry(30)
-		#self.fixed3.put(self.entry15, 210, 397)
 		
 		#Bottoni
 		self.button4 = gtk.Button("Inserisci")
@@ -208,8 +204,10 @@ class win6:
 		
 		self.table.show()
 		self.win.show_all()
+		
 	def apri(self, title):
 		file = finestre.file()
+		
 ##########Funzioni per Inserisci################################################
 		
 	def inserisci_vasca(self, obj):
@@ -230,35 +228,36 @@ class win6:
 		else:
 			#errore()
 			print "errore"
+		vasca = self.comboentry1.get_children()[0].get_text()
+		nome = self.entry6.get_text()
+		tipo = self.entry7.get_text()
+		filtro = self.entry8.get_text()
+		co2 = self.entry9.get_text()
+		illumi = self.entry10.get_text()
+		foto = self.entry11.get_text()
 		
-		self.ph = float(self.entry6.get_text())
-		self.kh = float(self.entry7.get_text())
-		self.gh = float(self.entry8.get_text())
-		self.no = float(self.entry9.get_text())
-		self.noo = float(self.entry10.get_text())
-		self.con = float(self.entry11.get_text())
-		self.am = float(self.entry12.get_text())
-		self.fe = float(self.entry13.get_text())
-		self.ra = float(self.entry14.get_text())
-		self.fo = float(self.entry15.get_text())
+		
+		
+		
 					
-		connessione = sqlite.connect("db")
+		connessione = sqlite.connect(as)
 		cursore = connessione.cursor()
-		cursore.execute("insert into test values (?,?,?,?,?,?,?,?,?,?,?,?)",(id, data, ph, kh, gh, no, noo, con, am, fe, ra, fo))
+		cursore.execute("insert into vasca values (?,?,?,?,?,?,?,?,?)",(id, vasca, data, nome, tipo, filtro, co2, illumi, foto))
 		connessione.commit()
 		#Finestra dialog con dati inseriti
-		window = finestre("Dati inseriti", "I dati sono stati inseriti con successo  ", "Chiudi")		
+		#window = finestre("Dati inseriti", "I dati sono stati inseriti con successo  ", "Chiudi")		
 		
 		self.spinbutton1.set_value(id+1)	
 		
 	def inserisci_2(self, obj):
-		connessione=sqlite.connect("db")
+		connessione=sqlite.connect(as)
 		cursore=connessione.cursor()
-		cursore.execute("select * from test")
+		cursore.execute("select * from vasca")
 		for x in cursore.fetchall():
+			print x[0]
 			self.spinbutton1.set_value(x[0]+1)
-	
-		self.menu_vasca_nomi()
+			
+			self.menu_vasca_nomi()
 	
 	def pulisci_vasca(self, obj):
 		self.entry6.set_text("")
@@ -267,10 +266,10 @@ class win6:
 		self.entry9.set_text("")
 		self.entry10.set_text("")
 		self.entry11.set_text("")
-		self.entry12.set_text("")
-		self.entry13.set_text("")
-		self.entry14.set_text("")
-		self.entry15.set_text("")
+		self.entry4.set_text("")
+		
+		
+		
 		
 	def data(self, widget, data=None):
 		data1.giorno()
@@ -283,101 +282,91 @@ class win6:
 		if page_number == 0:
 			print ""
 		elif page_number == 1: 
-			print ""
+			
 			self.tree_vasca()
 
 	
 		
 	def tree_vasca(self):
-		self.renderer = gtk.CellRendererText()
+		
+		
+		renderer = gtk.CellRendererText()
 		
 		i = 0
-		for c in ['Id', 'Data', 'Vasca', 'Ph', 'Kh', 'Gh', 'No2', 'No3', 'Conducibilità', 'Ammoniaca', 'Ferro', 'Rame', 'Fosfati']:
+		for c in ['Id', 'Vasca', 'Data', 'Nome', 'Tipo Acquario', 'Tipo Filtro', 'Impianto Co2', 'Illuminazione']:
 			self.column = gtk.TreeViewColumn(c, renderer, text=i)
 			i += 1
 			self.column.set_sort_column_id(i)
 			self.column.set_clickable(True)
 			self.column.set_resizable(True)
-			self.listview1.append_column(column)
+			self.treeview1.append_column(self.column)
 		
-	
-		connessione=sqlite.connect("db")
+		
+		connessione=sqlite.connect(as)
 		cursore=connessione.cursor()
-		cursore.execute("select * from test")
+		cursore.execute("select * from vasca")
 		for y in cursore.fetchall():
-		
-			connessione=sqlite.connect("db")
-			cursore=connessione.cursor()
-			cursore.execute("select * from vasca")
-			for a in cursore.fetchall():
-				self.listore.append([y[0], y[1], a[3], y[2], y[3], y[4], y[5], y[6], y[7], y[8], y[9], y[10], y[11]])
-################################################################################		
+			self.listore.append([y[0], y[1], y[2], y[3], y[4], y[5], y[6], y[7], y[8]])
+			
+################################################################################
 #########Funzioni per Cancella##################################################
+
 	def visualizza_vasca(self, obj):
 		
 		cercatest = self.spinbutton2.get_value()
-		connessione=sqlite.connect("db")
+		connessione=sqlite.connect(as)
 		cursore=connessione.cursor()
-		cursore.execute("select * from test")
+		cursore.execute("select * from vasca")
 		for a in cursore.fetchall(): 
 		
 			if (a[0]==cercatest):
-				self.entry32.set_text(str(a[0]))
-				self.entry33.set_text(str(a[1]))
-				self.entry34.set_text(str(a[2]))
-				self.entry35.set_text(str(a[3]))
-				self.entry36.set_text(str(a[4]))
-				self.entry37.set_text(str(a[5]))
-				self.entry38.set_text(str(a[6]))
-				self.entry39.set_text(str(a[7]))
-				self.entry40.set_text(str(a[8]))
-				self.entry41.set_text(str(a[9]))
-				self.entry42.set_text(str(a[10]))
-				self.entry43.set_text(str(a[11]))
+				self.entry17.set_text(str(a[0]))
+				self.entry18.set_text(str(a[3]))
+				self.entry19.set_text(str(a[4]))
+				self.entry20.set_text(str(a[5]))
+				self.entry21.set_text(str(a[6]))
+				self.entry22.set_text(str(a[7]))
+				
 		
 			else:
 				#Dire che c'è un errore e di riprovare (Finestra Dialog)
 				print ""
 			
 	def modifica_vasca(self, obj):
-		id = self.entry32.get_text()
-		data = self.entry33.get_text()
-		ph = self.entry34.get_text()
-		kh = self.entry35.get_text()
-		gh = self.entry36.get_text()
-		no = self.entry37.get_text()
-		noo = self.entry38.get_text()
-		con = self.entry39.get_text()
-		am = self.entry40.get_text()
-		fe = self.entry41.get_text()
-		ra = self.entry42.get_text()
-		fo = self.entry43.get_text()
+		id = self.entry17.get_text()
+		nome = self.entry18.get_text()
+		tipo = self.entry19.get_text()
+		filtro = self.entry20.get_text()
+		co2 = self.entry21.get_text()
+		illumi = self.entry22.get_text()
+		
 	
-		connessione=sqlite.connect("db")
+		connessione=sqlite.connect(as)
 		cursore=connessione.cursor()
-		cursore.execute("update test set ph= %(ph)s, kh = %(kh)s, gh = %(gh)s, no = %(no)s, noo = %(noo)s, con = %(con)s, amm = %(am)s, fe = %(fe)s, ra = %(ra)s, fo = %(fo)s" %vars())
+		cursore.execute("update vasca set a= %('nome')s, aa = %('tipo')s, b = %('filtro')s, c = %('co2')s, d = %('illumi')s" %vars())
 		connessione.commit()
 
 	def cancella_nuovo_vasca(self, obj):
 		id1 = self.spinbutton2.get_value()
-		connessione=sqlite.connect("db")
+		connessione=sqlite.connect(as)
 		cursore=connessione.cursor()
-		cursore.execute("delete from test where id= %(id1)s" %vars())
+		cursore.execute("delete from vasca where id= %(id1)s" %vars())
 		connessione.commit()
 		self.spinbutton2.set_value(id1-1)
 	
 	def cancella_attenzione_vasca(self, obj):
 		#Visualizza finestra dialog di conferma
-		connessione=sqlite.connect("db")
+		connessione=sqlite.connect(as)
 		cursore=connessione.cursor()
-		cursore.execute("delete from test")
+		cursore.execute("delete from vasca")
 		connessione.commit()
 		self.spinbutton2.set_value(0)
+		
 		#Visualizzare finestra di cancellazione avvenuta
 	
 		def menu_vasca_nomi():
 			data = []
-			connessione=sqlite.connect("db")
+			connessione=sqlite.connect(as)
 			cursore=connessione.cursor()
 			cursore.execute("select * from vasca")
 			for a in cursore.fetchall():
