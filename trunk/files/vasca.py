2e1fc47fc860ca6ce372c479d2b230a4cd54f103
stack_isteric <stack_isteric@14f0a01c-cf0a-0410-8b7d-a03980e64962>
2006-02-07 18:25:58 +0000

    Qualche miglioria al file vasca.. rimane da finire

--- trunk/files/vasca.py
+++ trunk/files/vasca.py
@@ -19,18 +19,20 @@
 #    along with Py-Acqua; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-import gtk
+import gtk, gobject
 import os
 from pysqlite2 import dbapi2 as sqlite
 
 class Vasca(gtk.Window):
 	def __init__(self): 
 		gtk.Window.__init__(self)
+		
 		self.set_title("Vasche")
+		self.set_size_request(600, 400)
 		
 		box = gtk.VBox()
 		
-		self.vasca_store = gtk.ListStore(int, str, str, str, str, str, str, str, str, gtk.gdk.Pixbuf)
+		self.vasca_store = gtk.ListStore(int, str, str, str, str, str, str, str, gtk.gdk.Pixbuf, str)
 		
 		self.view = view = gtk.TreeView(self.vasca_store)
 		
@@ -46,12 +48,13 @@ class Vasca(gtk.Window):
 			view.append_column(col)
 		
 		# Aggiungiamo la colonna per le immagini della vasca
-		col = gtk.TreeViewColumn("Immagine", gtk.CellRendererPixbuf(), pixbuf=9)
+		col = gtk.TreeViewColumn("Immagine", gtk.CellRendererPixbuf(), pixbuf=8)
 		col.set_resizable(True)
 		col.set_clickable(False)
 		view.append_column(col)
 		
 		view.get_selection().connect('changed', self.on_selection_changed)
+		view.connect('row-activated', self.on_row_activated)
 		
 		sw = gtk.ScrolledWindow()
 		sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
@@ -66,16 +69,12 @@ class Vasca(gtk.Window):
 		cursore.execute("select * from vasca")
 
 		# Costruisci l'immagine..
-		
 		for y in cursore.fetchall():
 			self.vasca_store.append([y[0], y[1], y[2], y[3], y[4],
-			y[5], y[6], y[7], y[8], None])
+			y[5], y[6], y[7], self.make_image(y[8]), y[8]])
 		
 		
-		# Usiamo un expander per rendere tutto piu' appetibile
-		self.exp = exp = gtk.Expander("<b>Editing</b>")
-		# Facciamo in modo che riconosca i tag di formattazione (<b>)
-		exp.set_use_markup(True)
+		frm = gtk.Frame("Editing:")
 		
 		# Creiamo una buttonbox per contenere i bottoni di modifica
 		bb = gtk.HButtonBox()
@@ -93,13 +92,10 @@ class Vasca(gtk.Window):
 		btn.connect('clicked', self.on_del)
 		bb.pack_start(btn)
 		
-		# Attacchiamo alla box expander e buttonbox
-		# in modo da non farli allargare durante il
-		# ridimensionamento :)
 		box.pack_start(bb, False, False, 0)
-		box.pack_start(exp, False, False, 0)
+		box.pack_start(frm, False, False, 0)
 		
-		# Creiamo la table che verra contenuta nell'expander
+		# Creiamo la table che verra contenuta nel frame
 		tbl = gtk.Table(8, 2)
 		
 		tbl.attach(self.new_label("Vasca:"), 0, 1, 0, 1)
@@ -137,10 +133,24 @@ class Vasca(gtk.Window):
 
 		tbl.set_border_width(10)
 		
-		exp.add(tbl)
+		frm.add(tbl)
+
+		self.status = gtk.Statusbar()
+		self.img = gtk.Image()
+		
+		hbox = gtk.HBox()
+		hbox.pack_start(self.img, False, False, 0)
+		hbox.pack_start(self.status)
+		
+		box.pack_start(hbox, False, False, 0)
 		
 		self.add(box)
 		self.show_all()
+
+		self.img.hide()
+		self.timeoutid = None
+
+		box.set_border_width(4)
 
 	def on_refresh(self, widget):
 		
@@ -161,11 +171,12 @@ class Vasca(gtk.Window):
 			tflt = self.e_filtro.get_text()
 			ico2 = self.e_co2.get_text()
 			illu = self.e_il.get_text()
+			img = self.e_path.get_text()
 			
 			conn = sqlite.connect(os.path.join('Data', 'db'))
 			cur = conn.cursor()
 
-			cur.execute("update vasca set t='%(text)s', da='%(date)s', a='%(name)s', aa='%(tacq)s', b='%(tflt)s', c='%(ico2)s', d='%(illu)s' where id = %(id)s" %vars())
+			cur.execute("update vasca set t='%(text)s', da='%(date)s', a='%(name)s', aa='%(tacq)s', b='%(tflt)s', c='%(ico2)s', d='%(illu)s', im='%(img)s' where id = %(id)s" %vars())
 			conn.commit()
 			
 			self.vasca_store.set_value(it, 1, text)
@@ -175,6 +186,10 @@ class Vasca(gtk.Window):
 			self.vasca_store.set_value(it, 5, tflt)
 			self.vasca_store.set_value(it, 6, ico2)
 			self.vasca_store.set_value(it, 7, illu)
+			self.vasca_store.set_value(it, 8, self.make_image(img))
+			self.vasca_store.set_value(it, 9, img)
+
+			self.update_status(0, "Row aggiornata (ID: %d)" % id)
 
 	def on_add(self, widget):
 		# Aggiungiamo dei valori casuali che andranno subito ad essere modificati
@@ -186,6 +201,7 @@ class Vasca(gtk.Window):
 		id = int(self.view.get_model().get_string_from_iter(it))
 		self.vasca_store.set_value(it, 0, id)
 
+		# TODO: Prendi i valori dalle entry
 		self.vasca_store.set_value(it, 1, "EDIT ME")
 		self.vasca_store.set_value(it, 2, "EDIT ME")
 		self.vasca_store.set_value(it, 3, "EDIT ME")
@@ -199,6 +215,8 @@ class Vasca(gtk.Window):
 
 		cur.execute('insert into vasca values(?,?,?,?,?,?,?,?,?)', tuple([id] + ["EDIT ME"] * 8))
 		conn.commit()
+
+		self.update_status(1, "Row aggiunta (ID: %d)" % id)
 		
 	def on_del(self, widget): 
 		# prendiamo l'iter selezionato e elimianiamolo dalla store
@@ -229,6 +247,8 @@ class Vasca(gtk.Window):
 					self.vasca_store.set_value(it, 0, tmp-1)
 				it = mod.iter_next(it)
 
+			self.update_status(2, "Row eliminata (ID: %d)" % value)
+
 	def on_selection_changed(self, sel):
 		# Aggiorniamo il contenuto delle entry in base alla selezione
 		mod, it = sel.get_selected()
@@ -241,8 +261,6 @@ class Vasca(gtk.Window):
 			self.e_filtro.set_text(mod.get_value(it, 5))
 			self.e_co2.set_text(mod.get_value(it, 6))
 			self.e_il.set_text(mod.get_value(it, 7))
-			
-			self.exp.set_expanded(True)
 		else:
 			self.e_vasca.set_text('')
 			self.e_data.set_text('')
@@ -251,8 +269,12 @@ class Vasca(gtk.Window):
 			self.e_filtro.set_text('')
 			self.e_co2.set_text('')
 			self.e_il.set_text('')
+			
+	def on_row_activated(self, tree, path, col):
+		mod = self.view.get_model()
+		it = mod.get_iter_from_string(str(path[0]))
 
-			self.exp.set_expanded(False)
+		InfoDialog(self, mod, it)
 
 	def new_label(self, txt):
 		lbl = gtk.Label()
@@ -261,3 +283,104 @@ class Vasca(gtk.Window):
 		lbl.set_alignment(0.0, 0.5)
 		
 		return lbl
+		
+	def make_image(self, name):
+		try:
+			print "Making image for file %s..." % name
+			pixbuf = gtk.gdk.pixbuf_new_from_file(os.path.join('Immagini', name))
+
+			# Facciamo lo scaling
+			return pixbuf.scale_simple(50, 50, gtk.gdk.INTERP_HYPER)
+		except:
+			return None
+	
+	def update_status(self, type, txt):
+		self.img.show()
+		
+		if type == 0:
+			self.img.set_from_stock(gtk.STOCK_SAVE, gtk.ICON_SIZE_MENU)
+		if type == 1:
+			self.img.set_from_stock(gtk.STOCK_ADD, gtk.ICON_SIZE_MENU)
+		if type == 2:
+			self.img.set_from_stock(gtk.STOCK_REMOVE, gtk.ICON_SIZE_MENU)
+		
+		self.status.pop(0)
+		self.status.push(0, txt)
+
+		if self.timeoutid != None:
+			gobject.source_remove(self.timeoutid)
+
+		self.timeoutid = gobject.timeout_add(2000, self.callback)
+	
+	def callback(self):
+		self.img.hide()
+		self.status.pop(0)
+
+		self.timeoutid = None
+		
+		return False
+
+class InfoDialog(gtk.Dialog):
+	def __init__(self, parent, mod, it):
+		gtk.Dialog.__init__(self, "Riepilogo", parent,
+			gtk.DIALOG_MODAL, (gtk.STOCK_OK, gtk.RESPONSE_OK))
+
+		self.set_size_request(400, 300)
+		self.vbox.set_border_width(10)
+
+		self.set_has_separator(False)
+		
+		tbl = gtk.Table(7, 2)
+		tbl.set_border_width(4)
+		
+		img = gtk.Image();
+		
+		try:
+			img.set_from_file(os.path.join('Immagini',
+				str(mod.get_value(it, 9))))
+		except:
+			img.set_from_stock(gtk.STOCK_IMAGE_MISSING,
+				gtk.ICON_SIZE_DIALOG)
+		
+		sw = gtk.ScrolledWindow()
+		sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+
+		sw.add_with_viewport(img)
+
+		self.vbox.pack_start(sw)
+		
+		tbl.attach(self.new_label("Vasca:"), 0, 1, 0, 1)
+		tbl.attach(self.new_label("Data:"), 0, 1, 1, 2)
+		tbl.attach(self.new_label("Nome:"), 0, 1, 2, 3)
+		tbl.attach(self.new_label("Tipo Acquario:"), 0, 1, 3, 4)
+		tbl.attach(self.new_label("Tipo Filtro:"), 0, 1, 4, 5)
+		tbl.attach(self.new_label("Impianto Co2:"), 0, 1, 5, 6)
+		tbl.attach(self.new_label("Illuminazione:"), 0, 1, 6, 7)
+
+		attach = lambda t, x, y: tbl.attach(gtk.Label(str(x)), 1, 2, x, y)
+		
+		attach(mod.get_value(it, 1), 0, 1)
+		attach(mod.get_value(it, 2), 1, 2)
+		attach(mod.get_value(it, 3), 2, 3)
+		attach(mod.get_value(it, 4), 3, 4)
+		attach(mod.get_value(it, 5), 4, 5)
+		attach(mod.get_value(it, 6), 5, 6)
+		attach(mod.get_value(it, 7), 6, 7)
+		
+		self.vbox.pack_start(tbl, False, False, 0)
+		self.show_all()
+
+		self.connect('response', self.on_response)
+	
+	def new_label(self, txt):
+		lbl = gtk.Label()
+		lbl.set_use_markup(True)
+		lbl.set_label('<b>' + txt + '</b>')
+		lbl.set_alignment(0.0, 0.5)
+		
+		return lbl
+		
+	def on_response(self, dial, id):
+		if id == gtk.RESPONSE_OK:
+			self.hide()
+			self.destroy()
