46c2b0e1bd69987bac3715b8bc487a88edb275ce
pyacqua <pyacqua@14f0a01c-cf0a-0410-8b7d-a03980e64962>
2006-02-09 19:31:02 +0000

    0.7

--- trunk/files/pesci.py
+++ trunk/files/pesci.py
@@ -72,8 +72,7 @@ class Pesci(gtk.Window):
 
 		# Costruisci l'immagine..
 		for y in cursore.fetchall():
-			self.vasca_store.append([y[0], y[1], y[2], y[3], y[4],
-			y[5], y[6], y[7], self.make_image(y[8]), y[8]])
+			self.vasca_store.append([y[0], y[1], y[2], y[3], y[4], self.make_image(y[8]), y[8]])
 		
 		
 		frm = gtk.Frame("Editing:")
