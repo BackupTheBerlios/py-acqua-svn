7be07d6f3e12f1c472c046ab470854c217916a3f
pyacqua <pyacqua@14f0a01c-cf0a-0410-8b7d-a03980e64962>
2006-02-15 18:56:13 +0000

    0.7

--- trunk/files/piante.py
+++ trunk/files/piante.py
@@ -106,7 +106,17 @@ class Piante(gtk.Window):
 		tbl.attach(self.new_label("Nome:"), 0, 1, 3, 4)
 		tbl.attach(self.new_label("Immagine:"), 0, 1, 7, 8)
 		
-		self.e_data, self.e_vasca, self.e_quantita = utils.DataButton(), gtk.Entry(), gtk.Entry()
+		liststore = gtk.ListStore(str)
+		self.e_vasca = gtk.ComboBox(liststore)
+		cell = gtk.CellRendererText()
+		self.e_vasca.pack_start(cell, True)
+		self.e_vasca.add_attribute(cell, 'text', 0)
+
+		# TODO: popola con i nomi delle vasche
+		liststore.append(["Deve prendere il nome della vasca dal db"])
+		
+		
+		self.e_data, self.e_quantita = utils.DataButton(), gtk.Entry()
 		self.e_nome = gtk.Entry()
 		self.e_path = gtk.Entry()
 
@@ -202,6 +212,7 @@ class Piante(gtk.Window):
 
 		# Settiamo il campo ID
 		self.piante_store.set_value(it, 0, id)
+
 		
 		date = self.e_data.get_text()
 		vasca = self.e_vasca.get_text()
