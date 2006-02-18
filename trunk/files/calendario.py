7bf477b565108cb06a01ff117dac0990ce3a9574
pyacqua <pyacqua@14f0a01c-cf0a-0410-8b7d-a03980e64962>
2006-02-18 17:02:34 +0000

    0.7

--- trunk/files/calendario.py
+++ trunk/files/calendario.py
@@ -22,63 +22,11 @@
 import pygtk
 pygtk.require('2.0')
 import gtk
-#import finestre
-from pysqlite2 import dbapi2 as sqlite
+
+
 
 
 class Calendario:
 	
-	def calendar_set_flags(self):
-		options = 0
-		for i in range(3):
-			if self.settings[i]:
-				options = options + (1<<i)
-		if self.window:
-			self.window.display_options(options)
-			
-	def calendar_toggle_flag(self, toggle):
-		j = 0
-		for i in range(3):
-			if self.flag_checkboxes[i] == toggle:
-				j = i
-				
-		self.settings[j] = not self.settings[j]
-		self.calendar_set_flags()
-		
-	def __init__(self):
-		connessione=sqlite.connect("Data/db")
-		cursore=connessione.cursor()
-		cursore.execute("select * from fertilizzante")		
-		
-		flags = [
-			"Mostra il mese/anno",
-			"Mostra i giorni"
-			]
-		self.window = None
-		self.flag_checkboxes = 5*[None]
-		self.settings = 5*[0]
-		self.marked_date = 31*[0]
-		
-		window = finestre.win(272, 280, "py-Acqua Calendario", 5)
-		
-		calendar = gtk.Calendar()
-		self.window = calendar
-		self.calendar_set_flags()
-		fixed = gtk.Fixed()
-		vbox = gtk.VBox()
-		window.add(fixed)
-		
-		fixed.put(calendar, 1, 1)
-		fixed.put(vbox, 5, 220)
-		
-		for x in cursore.fetchall():
-			day = x[4]
-			self.calendar.mark_day(day)
-		
-		for i in range(len(flags)):
-			toggle = gtk.CheckButton(flags[i])
-			toggle.connect("toggled", self.calendar_toggle_flag)
-			vbox.pack_start(toggle)
-			self.flag_checkboxes[i] = toggle
-			
-		window.show_all()
+	d = gtk.Calendar()
+	d.show_all()
