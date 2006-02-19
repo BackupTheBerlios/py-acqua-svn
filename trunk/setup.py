39426d68d176781a18f822d3279fe4b6f021ce53
stack_isteric <stack_isteric@14f0a01c-cf0a-0410-8b7d-a03980e64962>
2006-02-19 18:04:43 +0000

    Py2exe staff :P

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,12 +1,31 @@
-# Setup.py
+# setup.py
 from distutils.core import setup
 import py2exe
 import glob
 
+opts = {
+    "py2exe": {
+        "includes": "pango,atk,gobject",
+        "dll_excludes": [
+        "iconv.dll","intl.dll","libatk-1.0-0.dll",
+        "libgdk_pixbuf-2.0-0.dll","libgdk-win32-2.0-0.dll",
+        "libglib-2.0-0.dll","libgmodule-2.0-0.dll",
+        "libgobject-2.0-0.dll","libgthread-2.0-0.dll",
+        "libgtk-win32-2.0-0.dll","libpango-1.0-0.dll",
+        "libpangowin32-1.0-0.dll"],
+        }
+    }
+
 setup(
-	name="PyAcqua",
-	scripts["guy.py"],
-	data_files[
-		("pixmaps". glob.glob("pixmaps/*"))
-	]
+    name = "PyAcqua",
+    description = "Programma per la gestione degli acquari.",
+    version = "0.8",
+    windows = [
+        {"script": "gui.py",
+        "icon_resources": [(1, "pyacqua.ico")]
+        }
+    ],
+    options=opts,
+    data_files=[("pixmaps", glob.glob("pixmaps/*"))
+    ],
 )
