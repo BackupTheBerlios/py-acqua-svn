12bdbac67cfe844e1bfd8c7fd511661f71d32614
pyacqua <pyacqua@14f0a01c-cf0a-0410-8b7d-a03980e64962>
2006-02-26 15:12:38 +0000

    0.8

--- trunk/create-tran.sh
+++ trunk/create-tran.sh
@@ -3,7 +3,7 @@ if [ -f messages.pot ]; then
 	if [ -f $1 ]; then
 		echo "Il file $1 esiste gia'"
 	else
-		msginit -i messages.pot -l $1
+		msginit -i messages.po -l $1
 	fi
 else
 	echo "Prima esegui ./update-messages.sh"
