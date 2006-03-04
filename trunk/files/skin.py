459b914d1942e548fa6f26c21758b6cfe8bfe3b7
pyacqua <pyacqua@14f0a01c-cf0a-0410-8b7d-a03980e64962>
2006-03-04 18:13:55 +0000

    aggiunto lo skin

--- trunk/files/skin.py
+++ trunk/files/skin.py
@@ -45,16 +45,13 @@ class Skin(gtk.Window):
 		
 		box.pack_start(self.check)
 		
-		bb = gtk.HButtonBox()
-		bb.set_layout(gtk.BUTTONBOX_END)
-		bb.set_spacing(4)
 		
 		btn = gtk.Button(stock=gtk.STOCK_CLOSE)
 		btn.connect('clicked', self.exit)
 		bb.pack_start(btn)
 		
 		btn = gtk.Button(stock=gtk.STOCK_ADD)
-		btn.connect('clicked', self.inserisci_test)
+		#btn.connect('clicked', self.inserisci_test)
 		bb.pack_start(btn)
 		box.pack_start(bb, False, False, 0)
 		
@@ -62,44 +59,27 @@ class Skin(gtk.Window):
 		self.add(box)
 		self.show_all()
 		
+	def on_response(self, dialog, rsp_id):
+		if rsp_id == gtk.RESPONSE_NONE:
+			self.tip()
+		if rsp_id == gtk.RESPONSE_OK:
+			self.hide()
+			
+			if self.check.get_active():
+				impostazioni.show_tips = "0"
+			else:
+				impostazioni.show_tips = "1"
+
+			impostazioni.save()
+			
+			self.destroy()
+
+			TipsDialog.exist = False
 	
-	def inserisci_test(self, widget):
 	
-		impostazioni.minph = self.ph_minimo.get_text()
-		impostazioni.maxph = self.ph_massimo.get_text()
-		impostazioni.minkh = self.kh_minimo.get_text()
-		impostazioni.maxkh = self.kh_massimo.get_text()
-		impostazioni.mingh = self.gh_minimo.get_text()
-		impostazioni.maxgh = self.gh_massimo.get_text()
-		impostazioni.minno2 = self.no2_minimo.get_text()
-		impostazioni.maxno2 = self.no2_massimo.get_text()
-		impostazioni.minno3 = self.no3_minimo.get_text()
-		impostazioni.maxno3 = self.no3_massimo.get_text()
-		impostazioni.mincon = self.cond_minimo.get_text()
-		impostazioni.maxcon = self.cond_massimo.get_text()
-		impostazioni.minam = self.ammoniaca_minimo.get_text()
-		impostazioni.maxam = self.ammoniaca_massimo.get_text()
-		impostazioni.minfe = self.ferro_minimo.get_text()
-		impostazioni.maxfe = self.ferro_massimo.get_text()
-		impostazioni.minra = self.rame_minimo.get_text()
-		impostazioni.maxra = self.rame_massimo.get_text()
-		impostazioni.minfo = self.fosfati_minimo.get_text()
-		impostazioni.maxfo = self.fosfati_massimo.get_text()
-		
-		impostazioni.save()
 	
-		
-	def new_label(self, txt, bold=True):
-		lbl = gtk.Label()
-		if bold:
-			lbl.set_use_markup(True)
-			lbl.set_label('<b>' + txt + '</b>')
-			lbl.set_alignment(0, 0.5)
-		else:
-			lbl.set_label(txt)
-			lbl.set_alignment(0.5, 0.5)
-		
-		return lbl
+	
+	
 		
 	def exit(self, *w):
 		self.hide()
