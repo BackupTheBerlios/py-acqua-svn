cc43bc76d0e8bd16b801a0ede838a3323c0342c3
pyacqua <pyacqua@14f0a01c-cf0a-0410-8b7d-a03980e64962>
2006-07-13 21:06:20 +0000

    plugin fox

--- trunk/Plugin/fox.py
+++ trunk/Plugin/fox.py
@@ -34,27 +34,60 @@ class fox(gtk.Window):
 		box.set_spacing(4)
 		box.set_border_width(4)
 		
-		f1 = gtk.Frame(_('Sonde')); f2 = gtk.Frame(_('Uscite'))
+		f1 = gtk.Frame(_('Sonde')); f2 = gtk.Frame(_('Uscite')); f3 = gtk.Frame(_('Alba e Tramonto'))
 		
 		box.pack_start(f1, False, False, 0)
 		box.pack_start(f2, False, False, 0)
+		box.pack_start(f3, False, False, 0)
+		
+		tbl_sonde = gtk.Table(11, 3)
+		tbl_sonde.set_border_width(5)
+		
+		
+		tbl_sonde.attach(self.new_label(_('Ph')), 0, 1, 0, 1, yoptions=gtk.SHRINK)
+		tbl_sonde.attach(self.new_label(_('Temperatura')), 0, 1, 1, 2, yoptions=gtk.SHRINK)
+		
+		
+		
+		
 		
 		tbl = gtk.Table(11, 3)
 		tbl.set_border_width(5)
 		
-		tbl.attach(self.new_label(_('Sonde')), 1, 2, 0, 1, yoptions=gtk.SHRINK)
-		tbl.attach(self.new_label(_('Ph')), 1, 2, 1, 2, yoptions=gtk.SHRINK)
-		tbl.attach(self.new_label(_('Temperatura')), 1, 2, 2, 3, yoptions=gtk.SHRINK)
-		tbl.attach(self.new_label(_('Uscite')), 1, 2, 3, 4, yoptions=gtk.SHRINK)
-		tbl.attach(self.new_label(_('Uscita 1')), 1, 2, 4, 5, yoptions=gtk.SHRINK)
-		tbl.attach(self.new_label(_('Uscita 2')), 1, 2, 5, 6, yoptions=gtk.SHRINK)
-		tbl.attach(self.new_label(_('Uscita 3')), 1, 2, 6, 7, yoptions=gtk.SHRINK)
 		
-		box.pack_start(tbl)
+		labels = ('1', '2', '3', '4')
+		for i in labels:
+			widget = gtk.CheckButton(i)
+		
+		tbl.attach(self.new_label(_('Uscita 1')), 0, 1, 0, 1, yoptions=gtk.SHRINK)
+		tbl.attach(self.new_label(_('Uscita 2')), 0, 1, 1, 2, yoptions=gtk.SHRINK)
+		tbl.attach(self.new_label(_('Uscita 3')), 0, 1, 2, 3, yoptions=gtk.SHRINK)
+		tbl.attach(self.new_label(_('Uscita 4')), 0, 1, 3, 4, yoptions=gtk.SHRINK)
 		
 		
-		f1.add(tbl)
-		##### da finire perche da errori
+		tbl.attach(widget, 1, 2, 0, 1)
+		
+		
+		tbl_alba = gtk.Table(11, 3)
+		tbl_alba.set_border_width(5)
+		
+		tbl_alba.attach(self.new_label(_('Alba')), 0, 1, 0, 1, yoptions=gtk.SHRINK)
+		tbl_alba.attach(self.new_label(_('Tramonto')), 0, 1, 1, 2, yoptions=gtk.SHRINK)
+		
+		#qui pero e da modificare invece che scegliere la data bisogna far scegliere
+		# l ora dell alba e del tramonto.
+		
+		self.alba = files.utils.DataButton ()
+		self.tramonto = files.utils.DataButton ()
+		
+		tbl_alba.attach(self.alba, 1, 2, 0, 1, yoptions=0)
+		tbl_alba.attach(self.tramonto, 1, 2, 1, 2, yoptions=0)
+		
+		
+		f1.add(tbl_sonde)
+		f2.add(tbl)
+		f3.add(tbl_alba)
+		
 		self.add(box)
 		self.show_all ()
 			
