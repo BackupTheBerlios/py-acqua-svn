e571189ecae65dd4df1a7d5bedd55371097ace1a
pyacqua <pyacqua@14f0a01c-cf0a-0410-8b7d-a03980e64962>
2006-07-14 20:47:03 +0000

    modifica il file fox.py

--- trunk/Plugin/fox.py
+++ trunk/Plugin/fox.py
@@ -30,11 +30,11 @@ class fox(gtk.Window):
 		box.set_spacing(4)
 		box.set_border_width(4)
 		
-		f1 = gtk.Frame(_('Sonde')); f2 = gtk.Frame(_('Uscite')); f3 = gtk.Frame(_('Alba e Tramonto'))
+		f1 = gtk.Frame(_('Sonde')); f2 = gtk.Frame(_('Uscite'))#; f3 = gtk.Frame(_('Alba e Tramonto'))
 
 		box.pack_start (f1, False, False, 0)
 		box.pack_start (f2, False, False, 0)
-		box.pack_start (f3, False, False, 0)
+		#box.pack_start (f3, False, False, 0)
 		
 		fbox = gtk.VBox ()
 		f1_checks = list ()
@@ -56,7 +56,9 @@ class fox(gtk.Window):
 		for i in labels:
 			widget = gtk.CheckButton(_("Uscita ") + i)
 			f2_checks.append (widget)
-			tbl.attach (widget, 0, 1, x, x+1, yoptions=gtk.SHRINK); x += 1
+			tbl.attach (widget, x, x+1, 0, 1, yoptions=gtk.SHRINK); x += 1
+		
+		
 		
 		self.uscita1 = utils.Combo ()
 		self.uscita1.append_text(_("Filtro"))
@@ -69,28 +71,71 @@ class fox(gtk.Window):
 		
 
 		self.uscita2 = utils.Combo ()
+		self.uscita3 = utils.Combo ()
+		self.uscita4 = utils.Combo ()
 		
-		tbl.attach(self.uscita1, 1, 2, 0, 1, yoptions=0)
+		tbl.attach(self.uscita1, 0, 1, 1, 2, yoptions=0)
 		tbl.attach(self.uscita2, 1, 2, 1, 2, yoptions=0)
+		tbl.attach(self.uscita3, 2, 3, 1, 2, yoptions=0)
+		tbl.attach(self.uscita4, 3, 4, 1, 2, yoptions=0)
+		
+		tbl.attach(utils.new_label(_('Accensione')), 0, 1, 2, 3, yoptions=gtk.SHRINK)
+		tbl.attach(utils.new_label(_('Accensione')), 1, 2, 2, 3, yoptions=gtk.SHRINK)
+		tbl.attach(utils.new_label(_('Accensione')), 2, 3, 2, 3, yoptions=gtk.SHRINK)
+		tbl.attach(utils.new_label(_('Accensione')), 3, 4, 2, 3, yoptions=gtk.SHRINK)
+		
+		self.usc_1 = utils.DataButton ()
+		self.usc_2 = utils.DataButton ()
+		self.usc_3 = utils.DataButton ()
+		self.usc_4 = utils.DataButton ()
+		
+		tbl.attach(self.usc_1, 0, 1, 3, 4, yoptions=0)
+		tbl.attach(self.usc_2, 1, 2, 3, 4, yoptions=0)
+		tbl.attach(self.usc_3, 2, 3, 3, 4, yoptions=0)
+		tbl.attach(self.usc_4, 3, 4, 3, 4, yoptions=0)
+		
+		tbl.attach(utils.new_label(_('Spegnimento')), 0, 1, 4, 5, yoptions=gtk.SHRINK)
+		tbl.attach(utils.new_label(_('Spegnimento')), 1, 2, 4, 5, yoptions=gtk.SHRINK)
+		tbl.attach(utils.new_label(_('Spegnimento')), 2, 3, 4, 5, yoptions=gtk.SHRINK)
+		tbl.attach(utils.new_label(_('Spegnimento')), 3, 4, 4, 5, yoptions=gtk.SHRINK)
+		
+		self.usc_5 = utils.DataButton ()
+		self.usc_6 = utils.DataButton ()
+		self.usc_7 = utils.DataButton ()
+		self.usc_8 = utils.DataButton ()
+		
+		tbl.attach(self.usc_5, 0, 1, 5, 6, yoptions=0)
+		tbl.attach(self.usc_6, 1, 2, 5, 6, yoptions=0)
+		tbl.attach(self.usc_7, 2, 3, 5, 6, yoptions=0)
+		tbl.attach(self.usc_8, 3, 4, 5, 6, yoptions=0)
+		
+		self.co1 = gtk.CheckButton ("Co2 Permanente")
+		tbl.attach(self.co1, 0, 1, 6, 7, yoptions=0)
+		
+		self.co2 = gtk.CheckButton ("Co2 Regolata dal timer della luce")
+		tbl.attach(self.co2, 0, 1, 7, 8, yoptions=0)
+		
+		self.co3 = gtk.CheckButton ("Co2 Regolata dal pH")
+		tbl.attach(self.co3, 0, 1, 8, 9, yoptions=0)
 		
 		
-		tbl_alba = gtk.Table(2, 2)
-		tbl_alba.set_border_width(5)
+		#tbl_alba = gtk.Table(2, 2)
+		#tbl_alba.set_border_width(5)
 		
-		tbl_alba.attach(utils.new_label(_('Alba')), 0, 1, 0, 1, yoptions=gtk.SHRINK)
-		tbl_alba.attach(utils.new_label(_('Tramonto')), 0, 1, 1, 2, yoptions=gtk.SHRINK)
+		#tbl_alba.attach(utils.new_label(_('Alba')), 0, 1, 0, 1, yoptions=gtk.SHRINK)
+		#tbl_alba.attach(utils.new_label(_('Tramonto')), 0, 1, 1, 2, yoptions=gtk.SHRINK)
 		
 		#qui e da modificare! invece che scegliere la data bisogna far scegliere
 		# l ora dell alba e del tramonto.
 		
-		self.alba = utils.DataButton ()
-		self.tramonto = utils.DataButton ()
+		#self.alba = utils.DataButton ()
+		#self.tramonto = utils.DataButton ()
 		
-		tbl_alba.attach(self.alba, 1, 2, 0, 1, yoptions=0)
-		tbl_alba.attach(self.tramonto, 1, 2, 1, 2, yoptions=0)
+		#tbl_alba.attach(self.alba, 1, 2, 0, 1, yoptions=0)
+		#tbl_alba.attach(self.tramonto, 1, 2, 1, 2, yoptions=0)
 		
 		f2.add(tbl)
-		f3.add(tbl_alba)
+		#f3.add(tbl_alba)
 		
 		self.add(box)
 		self.connect ('delete_event', self.exit)
