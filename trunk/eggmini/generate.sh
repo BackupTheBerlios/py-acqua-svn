c16a6b0d7a6ce657b746bf08d34df238fc084c6d
pyacqua <pyacqua@14f0a01c-cf0a-0410-8b7d-a03980e64962>
2006-08-25 18:34:25 +0000

    tolto gli out

--- trunk/eggmini/generate.sh
+++ trunk/eggmini/generate.sh
@@ -2,4 +2,4 @@
 
 rm trayicon.c
 
-pygtk-codegen-2.0 --override trayicon.override --register /usr/share/pygtk/2.0/defs/gtk-types.defs --register /usr/share/pygtk/2.0/defs/gdk-types.defs --prefix pytrayicon trayicon.defs > trayicon.c
+pygtk-codegen-2.0 --override trayicon.override --register /usr/local/share/pygtk/2.0/defs/gtk-types.defs --register /usr/local/share/pygtk/2.0/defs/gdk-types.defs --prefix pytrayicon trayicon.defs > trayicon.c
--- trunk/eggmini/make.sh
+++ trunk/eggmini/make.sh
@@ -2,9 +2,9 @@
 
 rm -f *.so *.o
 
-gcc -c -fPIC `pkg-config --cflags gtk+-2.0 pygtk-2.0` -I/usr/include/python2.4 eggtrayicon.c
-gcc -c -fPIC `pkg-config --cflags gtk+-2.0 pygtk-2.0` -I/usr/include/python2.4 trayicon.c
-gcc -c -fPIC `pkg-config --cflags gtk+-2.0 pygtk-2.0` -I/usr/include/python2.4 trayiconmodule.c
+gcc -c -fPIC `pkg-config --cflags gtk+-2.0 pygtk-2.0` -I/usr/local/include/python2.4 eggtrayicon.c
+gcc -c -fPIC `pkg-config --cflags gtk+-2.0 pygtk-2.0` -I/usr/local/include/python2.4 trayicon.c
+gcc -c -fPIC `pkg-config --cflags gtk+-2.0 pygtk-2.0` -I/usr/local/include/python2.4 trayiconmodule.c
 gcc -shared -o trayicon.so -fPIC eggtrayicon.o trayicon.o trayiconmodule.o `pkg-config --libs gtk+-2.0`
 
 # all'ultimo va linkato alle gtk
