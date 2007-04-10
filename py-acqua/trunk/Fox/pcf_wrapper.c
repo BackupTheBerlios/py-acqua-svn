7c0ff939355225827dad469331b0736fe9b35e21
pyacqua <pyacqua@14f0a01c-cf0a-0410-8b7d-a03980e64962>
2007-04-10 19:54:41 +0000

    aagiunto il wrapper

--- py-acqua/trunk/Fox/pcf_wrapper.c
+++ py-acqua/trunk/Fox/pcf_wrapper.c
@@ -1,24 +1,24 @@
-/*#!/usr/bin/env python
-# -*- coding: iso-8859-15 -*- 
-#Copyright (C) 2005, 2006 Luca Sanna - Italy
-#http://pyacqua.altervista.org
-#email: pyacqua@gmail.com  
-#
-#   
-#Py-Acqua is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
-#
-#Py-Acqua is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with Py-Acqua; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+/*
+ * Copyright (C) 2005, 2006 Luca Sanna - Italy
+ * http://www.pyacqua.net
+ * email: info@pyacqua.net
+ * 
+ *    
+ * Py-Acqua is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * Py-Acqua is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Py-Acqua; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #include "stdio.h"     
 #include "unistd.h"    
 #include "sys/ioctl.h"
@@ -188,119 +188,145 @@ int i2c_outbyte(unsigned char x) {
 }
  
 /* Inizio codice wrapper */
- 
+
 static PyObject * wrap_i2c_getbit (PyObject *self, PyObject *args)
 {
 	return Py_BuildValue ("i", i2c_getbit ());
 }
- 
-
-static PyMethodDef Foxi2cMethods[] = {
-	
-	{"i2c_getbit", wrap_i2c_getbit, METH_VARARGS, "Get the SDA line state"},
-	{NULL, NULL, 0, NULL} // questa ha lo scopo di chiudere la tabella delle funzioni esportate
-};
- 
 
 /* i2c_dir_out */
 static PyObject * wrap_i2c_dir_out (PyObject *self, PyObject *args)
 {
-	return Py_BuildValue ("i", i2c_dir_out ());
+	i2c_dir_out ();
+	
+	// void i2c_dir_out(void) questa ritorna void quindi torniamo un None anche a python
+	Py_INCREF(Py_None);
+	return Py_None;
 }
-static PyMethodDef Foxi2cMethods[] = {
-	{"i2c_dir_out", wrap_i2c_dir_out, METH_VARARGS, "Set the SDA line as output"},
-	{NULL, NULL, 0, NULL}
-};
 
 /* i2c_dir_in */
 static PyObject * wrap_i2c_dir_in (PyObject *self, PyObject *args)
 {
-	return Py_BuildValue ("i", i2c_dir_in ());
+	// idem per questa giu
+	i2c_dir_in ();
+	
+	Py_INCREF(Py_None);
+	return Py_None;
 }
-static PyMethodDef Foxi2cMethods[] = {
-	{"i2c_dir_in", wrap_i2c_dir_in, METH_VARARGS, "Set the SDA line as input"},
-	{NULL, NULL, 0, NULL}
-};
 
 /* i2c_data */
 static PyObject * wrap_i2c_data (PyObject *self, PyObject *args)
 {
-	return Py_BuildValue ("i", i2c_data ());
+	// qui e' un po diverso perche' sta funzione prende un intero come argomento
+	// void i2c_data(int state) il prototipo e' questo quindi dobbiamo convertire
+	// args.
+	
+	int argomento;
+	if (!PyArg_ParseTuple (args, "i", &argomento)) // questa provvede a convertire l'args in un int
+		return NULL; // se ci sono errori usciamo e nn chiamiamo un cazzo
+	
+	// altrimenti chiamiamo la funzione
+	i2c_data (argomento);
+	
+	// e torniamo il nostro void al python
+	Py_INCREF(Py_None);
+	return Py_None;
 }
-static PyMethodDef Foxi2cMethods[] = {
-	{"i2c_data", wrap_i2c_data, METH_VARARGS, "Set the SDA line state"},
-	{NULL, NULL, 0, NULL}
-};
 
 /* i2c_clk */
 static PyObject * wrap_i2c_clk (PyObject *self, PyObject *args)
 {
-	return Py_BuildValue ("i", i2c_clk ());
+	// lo stesso a quella di sopra per questa funzione. Il suo prototipo e'
+	// void i2c_clk(int state)
+	
+	int argomento;
+	if (!PyArg_ParseTuple (args, "i", &argomento))
+		return NULL;
+	
+	i2c_clk (argomento);
+	
+	Py_INCREF(Py_None);
+	return Py_None;
 }
-static PyMethodDef Foxi2cMethods[] = {
-	{"i2c_clk", wrap_i2c_clk, METH_VARARGS, "Set the SCL line state"},
-	{NULL, NULL, 0, NULL}
-};
-
 /* i2c_inbyte */
 static PyObject * wrap_i2c_inbyte (PyObject *self, PyObject *args)
 {
-	return Py_BuildValue ("i", i2c_inbyte ());
+	// ecco mo son cazzi. Il prototipo e'
+	// unsigned char i2c_inbyte(int islast)
+	// per l'argomento e' sempre la stessa roba di prima
+	
+	int argomento;
+	if (!PyArg_ParseTuple (args, "i", &argomento))
+		return NULL;
+	
+	// solo che dobbiamo anche tornare un unsigned char
+	// http://www.python.org/doc/1.5.2p2/ext/buildValue.html
+	// l'unsigned nn c'e' vabbe poi lo converti da python al massimo per ora lascia cosi'
+	
+	return Py_BuildValue ("b", i2c_inbyte (argomento));
 }
-static PyMethodDef Foxi2cMethods[] = {
-	{"i2c_inbyte", wrap_i2c_inbyte, METH_VARARGS, "Read a byte from I2C bus and send the ack sequence"},
-	{NULL, NULL, 0, NULL}
-};
 
 /* i2c_open */
 static PyObject * wrap_i2c_open (PyObject *self, PyObject *args)
 {
 	return Py_BuildValue ("i", i2c_open ());
 }
-static PyMethodDef Foxi2cMethods[] = {
-	{"i2c_open", wrap_i2c_open, METH_VARARGS, "Open the GPIOB dev"},
-	{NULL, NULL, 0, NULL}
-};
 
 /* i2c_close */
 static PyObject * wrap_i2c_close (PyObject *self, PyObject *args)
 {
-	return Py_BuildValue ("i", i2c_close ());
+	i2c_close ();
+	
+	Py_INCREF(Py_None);
+	return Py_None;
 }
-static PyMethodDef Foxi2cMethods[] = {
-	{"i2c_close", wrap_i2c_close, METH_VARARGS, "Close the GPIOB dev"},
-	{NULL, NULL, 0, NULL}
-};
 
 /* i2c_start */
 static PyObject * wrap_i2c_start (PyObject *self, PyObject *args)
 {
-	return Py_BuildValue ("i", i2c_start ());
+	i2c_start ();
+	
+	Py_INCREF(Py_None);
+	return Py_None;
 }
-static PyMethodDef Foxi2cMethods[] = {
-	{"i2c_start", wrap_i2c_start, METH_VARARGS, "Send a start sequence to I2C bus"},
-	{NULL, NULL, 0, NULL}
-};
 
 /* i2c_stop */
 static PyObject * wrap_i2c_stop (PyObject *self, PyObject *args)
 {
-	return Py_BuildValue ("i", i2c_stop ());
+	i2c_stop ();
+	
+	Py_INCREF(Py_None);
+	return Py_None;
 }
-static PyMethodDef Foxi2cMethods[] = {
-	{"i2c_stop", wrap_i2c_stop, METH_VARARGS, "Send a stop sequence to I2C bus"},
-	{NULL, NULL, 0, NULL}
-};
 
 /* i2c_outbyte */
 static PyObject * wrap_i2c_outbyte (PyObject *self, PyObject *args)
 {
-	return Py_BuildValue ("i", i2c_outbyte ());
+	// arriviamo alla tosta
+	// int i2c_outbyte(unsigned char x)
+	
+	char argomento;
+	if (!PyArg_ParseTuple (args, "b", &argomento))
+		return NULL;
+	
+	return Py_BuildValue ("i", i2c_outbyte ((unsigned char)argomento));
 }
+
 static PyMethodDef Foxi2cMethods[] = {
+	{"i2c_inbyte", wrap_i2c_inbyte, METH_VARARGS, "Read a byte from I2C bus and send the ack sequence"},
+	{"i2c_data", wrap_i2c_data, METH_VARARGS, "Set the SDA line state"},
+	{"i2c_clk", wrap_i2c_clk, METH_VARARGS, "Set the SCL line state"},
+	{"i2c_open", wrap_i2c_open, METH_VARARGS, "Open the GPIOB dev"},
+	{"i2c_close", wrap_i2c_close, METH_VARARGS, "Close the GPIOB dev"},
+	{"i2c_start", wrap_i2c_start, METH_VARARGS, "Send a start sequence to I2C bus"},
+	{"i2c_stop", wrap_i2c_stop, METH_VARARGS, "Send a stop sequence to I2C bus"},
+	{"i2c_getbit", wrap_i2c_getbit, METH_VARARGS, "Get the SDA line state"},
+	{"i2c_dir_in", wrap_i2c_dir_in, METH_VARARGS, "Set the SDA line as input"},
+	{"i2c_dir_out", wrap_i2c_dir_out, METH_VARARGS, "Set the SDA line as output"},
 	{"i2c_outbyte", wrap_i2c_outbyte, METH_VARARGS, "Send a byte to the I2C bus and return the ack sequence from slave rtc"},
 	{NULL, NULL, 0, NULL}
 };
+
 /* Ora passiamo a definire l'ultima funzione che serve ad inizializzare l'intero modulo */
 PyMODINIT_FUNC
 initfoxi2c (void)
