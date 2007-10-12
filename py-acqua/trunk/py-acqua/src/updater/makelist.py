122440e726c9a50f5ca5d6f43f9f14228e2e53b1
stack_isteric <stack_isteric@14f0a01c-cf0a-0410-8b7d-a03980e64962>
2007-10-12 15:28:41 +0000

    first commit - Updater

---
+++ py-acqua/trunk/py-acqua/src/updater/makelist.py
@@ -0,0 +1,194 @@
+import sys
+import base64
+import ConfigParser
+
+from optparse import OptionParser
+from updater import DatabaseWrapper
+from xml.dom.minidom import parse, parseString, getDOMImplementation
+
+class ListCreator(object):
+	def __init__(self, program, database, info, output):
+		self.db = DatabaseWrapper(database)
+
+		self.info = info
+		self.output = output
+		self.program = program
+
+		self.options = {
+			'%s.uselast'           % self.program : True,
+			'%s.mainversion'       % self.program : 0,
+			'%s.secondversion'     % self.program : 0,
+			'%s.revision'          % self.program : 0,
+			'%s.changelog'         % self.program : '',
+			'%s.database'          % self.program : '',
+			'%s.message'           % self.program : '',
+
+			'%s.message-pre'       % self.program : '',
+			'%s.message-post'      % self.program : '',
+
+			'%s.mirrors'           % self.program : [],
+
+			'%s.downloads-windows' % self.program : [],
+			'%s.downloads-tarball' % self.program : [],
+			'%s.downloads-svn'     % self.program : [],
+
+			'%s.actions-pre'       % self.program : [],
+			'%s.actions-post'      % self.program : [],
+
+		}
+
+		self.readInfoFile()
+
+		if self.options['%s.uselast' % self.program]:
+			self.readVersionFromDatabase()
+	
+	def readVersionFromDatabase(self):
+		try:
+			self.options['%s.mainversion' % self.program], self.options['%s.secondversion' % self.program], self.options['%s.revision' % self.program] = map(
+				int, self.db.select("SELECT mainversion, version, revision FROM program WHERE name=\"%s\"" % self.db.sanitize(self.program))[0]
+			)
+		except:
+			print "Cannot get the versions for program %s." % self.program
+			sys.exit(-1)
+	
+	def readInfoFile(self):
+		parser = ConfigParser.ConfigParser()
+		parser.read(self.info)
+
+		for sec in parser.sections():
+			for opt in parser.options(sec):
+				self.handleOption(sec.lower(), opt.lower(), parser.get(sec, opt))
+	
+	def handleOption(self, sec, opt, value):
+		if sec in self.options:
+			# Mirrors or lists to handle
+
+			# TODO: evalutate numbers
+
+			check = lambda x, y: x[:len(y)] == y and ((len(x[len(y):]) > 0 and x[len(y):].isdigit()) or (len(x[len(y):]) == 0))
+
+			if check(opt, "mirror"):
+				self.options["%s.mirrors" % self.program].append(value)
+
+			elif check(opt, "svn"):
+				self.options["%s.downloads-svn" % self.program].append(value)
+			elif check(opt, "tarball"):
+				self.options["%s.downloads-tarball" % self.program].append(value)
+			elif check(opt, "windows"):
+				self.options["%s.downloads-windows" % self.program].append(value)
+
+			elif check(opt, "pre"):
+				self.options["%s.actions-pre" % self.program].append(value)
+			elif check(opt, "post"):
+				self.options["%s.actions-post" % self.program].append(value)
+		else:
+			# type checking
+
+			full = ".".join((sec, opt))
+			
+			if full not in self.options:
+				print "%s not used." % full
+				return
+
+			converter = type(self.options[full])
+			
+			# Necessary ugly code :D
+
+			def strict_bool(x):
+				if x == "1" or x.lower() == "true":
+					return True
+				elif x == "0" or x.lower() == "false":
+					return False
+				else:
+					raise
+
+			if converter == bool:
+				converter = strict_bool
+
+			try:
+				if value != "":
+					self.options[full] = converter(value)
+			except:
+				print "Type error detected:"
+				print "%s must be of %s not %s" % (full, (converter == strict_bool) and (bool) or (converter), type(value))
+				
+				sys.exit(-1)
+	
+	def create(self):
+		doc = getDOMImplementation().createDocument(None, "%s-update" % self.program, None)
+		
+		element = doc.createElement("update")
+		doc.documentElement.appendChild(element)
+
+		def temp(x):
+			f = open(x, 'r')
+			t = base64.b64encode(f.read())
+			f.close()
+			return t
+
+		values = [
+			# optionname category howto dump
+			['mainversion', 	'info', 	str],
+			['secondversion',	'info', 	str],
+			['revision', 		'info', 	str],
+			['message', 		'info', 	str],
+
+			['changelog', 		'changelog',	temp],
+			
+			['database', 		None, 		str],
+
+			['message-pre', 	'info',	str],
+			['message-post',	'info',	str],
+
+			['mirrors',		'mirrors',	None,	'url'],
+
+			['actions-pre',		'actions',	None,	'pre'],
+			['actions-post',	'actions',	None,	'post'],
+
+			['downloads-svn',	'downloads',	None,	'svn'],
+			['downloads-tarball',	'downloads',	None,	'tarball'],
+			['downloads-windows',	'downloads',	None,	'windows']
+
+		]
+
+		self.categories = {}
+
+		for option in values:
+			if len(option) == 4:
+				if not option[1] in self.categories:
+					t = self.categories[option[1]] = doc.createElement(option[1])
+					element.appendChild(t)
+				
+				t = self.categories[option[1]]
+				
+				for i in self.options["%s.%s" % (self.program, option[0])]:
+					x = doc.createElement(option[3])
+					x.appendChild(doc.createTextNode(i))
+					t.appendChild(x)
+			else:
+				if option[1] == None:
+					t = doc.createElement(option[0])
+					t.appendChild(doc.createTextNode(str(self.options["%s.%s" % (self.program, option[0])])))
+					element.appendChild(t)
+				else:
+					if not option[1] in self.categories:
+						t = self.categories[option[1]] = doc.createElement(option[1])
+						element.appendChild(t)
+					
+					t = self.categories[option[1]]
+					x = doc.createElement(option[0])
+					
+					x.appendChild(doc.createTextNode(option[2](self.options["%s.%s" % (self.program, option[0])])))
+					t.appendChild(x)
+
+		doc.writexml (sys.stdout, "", "", "")
+
+if __name__ == "__main__":
+	parser = OptionParser("%s makelist <program> <database> <info-file> <output-xml>" % sys.argv[0])
+	
+	(options, args) = parser.parse_args()
+
+	if len(args) != 5:
+		parser.print_help()
+	else:
+		ListCreator(args[1], args[2], args[3], args[4]).create()
---
+++ py-acqua/trunk/py-acqua/src/updater/updater.py
@@ -0,0 +1,311 @@
+import os
+import md5
+import sys
+
+from optparse import OptionParser
+from pysqlite2 import dbapi2 as sqlite
+from os.path import getsize
+
+class DatabaseWrapper(object):
+	def __init__(self, database):
+		self.connection = sqlite.connect(database)
+		self.cursor = self.connection.cursor()
+	
+	def sanitize(self, str):
+		# TODO: more check?
+
+		if type(str) == tuple:
+			t = []
+			for i in str:
+				if t and type(t) == str:
+					t.append(i.replace("'", "''"))
+				else:
+					t.append(i)
+			return tuple(t)
+		else:
+			return str.replace("'", "''")
+	
+	def select(self, req):
+		self.cursor.execute(req)
+		return self.cursor.fetchall()
+	
+	def execute(self, req, commit=True):
+		self.cursor.execute(req)
+		
+		if commit:
+			self.connection.commit()
+
+class DatabaseUpdater(DatabaseWrapper):
+	def __init__(self, program, path, database, main=None, ver=None, rev=None):
+
+		DatabaseWrapper.__init__(self, database)
+
+		self.program = program
+		self.path = path
+		
+		self.database = database
+
+		self.v_main = main
+		self.v_ver = ver
+		self.v_rev = rev
+
+		self.checkDatabaseSchema()
+	
+	def checkDatabaseSchema(self):
+
+		tables = [
+			'directory', 'file', 'program'
+		]
+
+		schemas = [
+			'CREATE TABLE directory(id INTEGER PRIMARY KEY, name TEXT, revision INTEGER, filenum INTEGER, program_id INTEGER)',
+			'CREATE TABLE file(id INTEGER PRIMARY KEY, name TEXT, revision INTEGER, bytes REAL, md5 VARCHAR(32), directory_id INTEGER, program_id INTEGER)',
+			'CREATE TABLE program(id INTEGER PRIMARY KEY, name TEXT, mainversion INTEGER, version INTEGER, revision INTEGER, dirnum INTEGER)'
+		]
+
+		unused = []
+
+		ret = self.select("select * from sqlite_master")
+
+		for i in ret:
+			if i[4] not in schemas:
+				print "Wrong schema in database."
+				print "I know who is the killer :o"
+				print "Table: %s" % i[2]
+				
+				if i[2] in tables:
+					print "Wrong schema detected."
+					print i[4]
+					print " .. should be .."
+					print schemas[tables.index(i[2])]
+
+				sys.exit(0)
+			else:
+				unused.append(schemas.index(i[4]))
+
+
+			if i[2] not in tables:
+				print "non-necessary Table: %s" % i[2]
+
+		for i in range(len(schemas)):
+			if i not in unused:
+				self.execute(schemas[i])
+	
+	def checkArgs(self):
+		if self.v_main == None or self.v_ver == None or self.v_rev == None:
+			print "Error in parameters: main, version and revision cannot be None"
+			sys.exit(-1)
+	
+	def updateProgramTable(self, noupdate=False):
+		ret = self.select("SELECT * FROM program WHERE name=\"%s\"" % self.sanitize(self.program))
+
+		if len(ret) > 1:
+			print "Error in database. Duplicated entries for program %s." % self.program
+		elif len(ret) == 1:
+			# must update
+
+			id   = ret[0][0]
+
+			if not noupdate:
+				main = (self.v_main) and (self.v_main) or (ret[0][2])
+				ver  = (self.v_ver)  and (self.v_ver)  or (ret[0][3])
+				rev  = (self.v_rev)  and (self.v_rev)  or (ret[0][4])
+
+				self.execute("UPDATE program SET mainversion=%d, version=%d, revision=%d WHERE id=%d" % (main, ver, rev, id))
+
+			return id
+		else:
+			# insert rapido
+
+			self.checkArgs()
+				
+			self.execute("INSERT INTO program VALUES(NULL, \"%s\", %d, %d, %d, 0)" % self.sanitize
+				(
+					(self.program, self.v_main, self.v_ver, self.v_rev)
+				)
+			)
+
+			return self.updateProgramTable(noupdate=True)
+	
+	def updateDirectoryTable(self, dir, p_id, noupdate=False):
+		# Dovremmo controllare l'esistenza
+		# se esiste aggiornale la revision se non ci e' stato passato nulla come arg
+		# 	altrimenti forzare sui valori passati
+		# se non esiste inserire i valori passati
+		# 	se non ci sono errore ed esci -1
+		
+		ret = self.select("SELECT * FROM directory WHERE name=\"%s\" AND program_id=%d" % self.sanitize((dir, p_id)))
+
+		if len(ret) > 1:
+			print "Error in database. Duplicated entries for directory %s with program_id = %d." % (dir, p_id)
+
+		elif len(ret) == 1:
+
+			id   = ret[0][0]
+
+			if not noupdate:
+				rev = (self.v_rev) and (self.v_rev) or (ret[0][2])
+				self.execute("UPDATE directory SET revision=%d WHERE id=%d" % (rev + 1, id))
+
+			return id
+		else:
+
+			self.checkArgs()
+
+			self.execute("INSERT INTO directory VALUES(NULL, \"%s\", %d, 0, %d)" % self.sanitize
+				(
+					(dir, self.v_rev, p_id)
+				)
+			)
+
+			return self.updateDirectoryTable(dir, p_id, noupdate=True)
+	
+	def updateDirectoryFileN(self, dirid, nfiles):
+		self.execute("UPDATE directory SET filenum=%d WHERE id=%d" % (nfiles, dirid))
+	
+	def md5sum(self, file):
+		fobj = open(file, 'rb')
+		m = md5.new()
+
+		while True:
+			d = fobj.read(8096)
+			if not d: break
+			m.update(d)
+
+		return m.hexdigest()
+
+	def updateFileTable(self, file, p_id, dirid, dirname, noupdate=False):
+		
+		# Dovremmo controllare l'esistenza
+		# se esiste aggiornale la revision se non ci e' stato passato nulla come arg e se md5 e minchiate son variate e propagare alla dir
+		# 	altrimenti forzare sui valori passati
+		# se non esiste inserire i valori passati
+		# 	se non ci sono errore ed esci -1
+		
+		ret = self.select("SELECT * FROM file WHERE name=\"%s\" AND program_id=%d" % self.sanitize((file, p_id)))
+
+		if len(ret) > 1:
+			print "Error in database. Duplicated entries for file %s with program id = %d." % (file, p_id)
+
+		elif len(ret) == 1:
+
+			id   = ret[0][0]
+
+			if not noupdate:
+				rev   = (self.v_rev) and (self.v_rev) or (ret[0][2] + 1)
+				
+				bytes = ret[0][3]
+				sumd5 = ret[0][4]
+				did   = ret[0][5]
+
+				c_bytes = getsize(file)
+				c_sumd5 = self.md5sum(file)
+				c_did   = dirid
+
+				if c_bytes != bytes or c_sumd5 != sumd5 or c_did != did:
+					self.execute("UPDATE file SET revision=%d, bytes=%d, md5=\"%s\", directory_id=%d, program_id=%d WHERE id=%d" % self.sanitize
+						(
+							(rev, c_bytes, c_sumd5, c_did, p_id, id)
+						)
+					)
+
+					self.updateDirectoryTable(dirname, dirid)
+
+
+			return id
+		else:
+
+			self.checkArgs()
+
+			self.execute("INSERT INTO file VALUES(NULL, \"%s\", %d, %d, \"%s\", %d, %d)" % self.sanitize
+				(
+					(file, self.v_rev, getsize(file), self.md5sum(file), dirid, p_id)
+				)
+			)
+
+			return self.updateFileTable(file, p_id, dirid, dirname, noupdate=True)
+	
+	def scanPath(self, programid):
+		# Facciamo uno scan nella path in modo ricorsivo
+
+		os.chdir(self.path)
+		
+		stack = ["."]
+
+		ndir = 1
+		nfiles = 0
+
+		dirid = 0
+		isfirst = True
+
+		while stack:
+			
+			if isfirst:
+				self.updateDirectoryFileN(dirid, nfiles)
+				nfiles = 0
+				
+			dir = stack.pop()
+
+			# Non aggiorniamo la revision. Se i file cambiano verra propagata.
+			dirid = self.updateDirectoryTable(dir, programid, True)
+
+			isfirst = False
+
+			for file in os.listdir(dir):
+				fullname = os.path.join(dir, file)
+
+				if not os.path.isdir(fullname):
+					self.updateFileTable(fullname, programid, dirid, dir)
+					nfiles += 1
+				
+				elif not os.path.islink(fullname):
+					if file != ".svn":
+						stack.append(fullname)
+						ndir +=1
+		
+		print
+		print "Id\tmain\tversion\trev\tdirnum\tname"
+		print "--------------------------------------------"
+		
+		for i in self.select("SELECT * FROM program"):
+			print "%d\t%d\t%d\t%d\t%d\t%s" % (i[0], i[2], i[3], i[4], i[5], i[1])
+
+		print
+		print "Id\trev\tnfiles\tpid\tname"
+		print "------------------------------------"
+
+		for i in self.select("SELECT * FROM directory"):
+			print "%d\t%d\t%d\t%d\t%s" % (i[0], i[2], i[3], i[4], i[1])
+
+		print
+		print "Id\trev\tbytes\tmd5sum\t\t\t\t\tdir\tpid\tname"
+		print "------------------------------------------------------------------------------------"
+
+		for i in self.select("SELECT * FROM file"):
+			print "%d\t%d\t%d\t%s\t%d\t%d\t%s" % (i[0], i[2], i[3], i[4], i[5], i[6], i[1])
+
+		print
+	
+	def update(self):
+		id = self.updateProgramTable()
+		self.scanPath(id)
+
+if __name__ == "__main__":
+	parser = OptionParser("%s <program> <path> <database>" % sys.argv[0])
+
+	parser.add_option("-m", "--main", dest="main_version", type="int",
+		help="the main version for program")
+	parser.add_option("-v", "--version", dest="version", type="int",
+		help="the second version for program")
+	parser.add_option("-r", "--revision", dest="revision", type="int",
+		help="the revision for program")
+	
+	(options, args) = parser.parse_args()
+
+	if len(args) != 3:
+		parser.print_help()
+	else:
+		DatabaseUpdater(
+			args[0], args[1], args[2],
+			options.main_version, options.version, options.revision
+		).update()
