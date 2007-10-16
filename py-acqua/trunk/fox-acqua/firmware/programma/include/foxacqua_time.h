1333603180d9e1b1cff04df2ad134a2fc203ea2f
claudyus <claudyus@14f0a01c-cf0a-0410-8b7d-a03980e64962>
2007-10-16 20:25:34 +0000

    Aggiunta lettura/scrittura timers da file

---
+++ py-acqua/trunk/fox-acqua/firmware/programma/include/foxacqua_time.h
@@ -0,0 +1,50 @@
+//#Copyright (C) 2005, 2007 Py-acqua
+//#http://www.pyacqua.net
+//#
+//#email: info@pyacqua.net
+//#
+//#
+//#Py-Acqua is free software; you can redistribute it and/or modify
+//#    it under the terms of the GNU General Public License as published by
+//#    the Free Software Foundation; either version 2 of the License, or
+//#    (at your option) any later version.
+//#
+//#Py-Acqua is distributed in the hope that it will be useful,
+//#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//#    GNU General Public License for more details.
+//#
+//#    You should have received a copy of the GNU General Public License
+//#    along with Py-Acqua; if not, write to the Free Software
+//#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//#
+//#    Gestione timer tramite file
+
+#define TIMER_FILE "/usr/local/var/fox-acqua/fox_timer.lst"
+
+#define N_SONDE 7
+#define N_TIMER 10
+
+#define RECORD sizeof(char)+sizeof(char)+sizeof(int)+sizeof(char)
+
+FILE timer_fd=NULL;
+
+struct v_timer {
+	char num;		//numero del timer, un numero tra 0 e N_SONDE * N_TIMER
+	char pin;
+	int ora;
+	char stato;
+} typedef s_timer *;
+
+
+//Legge l'n-essimo time e salva i valori nella struttura passata per puntatore
+int read_timer(s_timer *, int );
+//Legge tutti i time e salva i valori nell'array di trutture passati per puntatore
+int read_timers(s_timer **);
+//Salva l'n-essimo time su file
+int set_timer(s_timer *);
+//Salva tutti i time su file
+int set_timers(s_timer **);
+//Resetta il timer n-essimo
+int del_timer(int)
+static int timer_file(int );
